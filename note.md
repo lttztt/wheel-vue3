81c89372e7b95a320b4c3c6f737da66fd8685590
liuzhe <liuzhexx@foxmail.com>
2020-07-30 18:48:04 +0800

    初始化项目

---
+++ note.md
@@ -0,0 +1,6 @@
+## vue3.0
+
+- index.html文件中引入js文件使用了`type="module"`
+- vue3支持在template有多个根标签
+- vue3 的 `CreateApp()`, vue2是 new Vue()
+- `CreateApp(组件)`, `new Vue({template, render})`
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Lqve/>
+</template>
+
+<script>
+import Lqve from './components/Lqve.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Lqve
+  }
+}
+</script>
