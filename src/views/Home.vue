29ce305f5ddec63fc2e6b4b8bd653d571406705c
liuzhe <liuzhexx@foxmail.com>
2020-07-30 19:57:59 +0800

    home页的样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <Topnav/>
-        <div class="bannber">
+        <Topnav />
+        <div class="banner">
             <h1>禅UI</h1>
             <h2>一个随性的UI框架</h2>
             <p class="actions">
@@ -12,12 +12,33 @@
     </div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
-  components: {
-    Topnav
-  }
-}
+    components: {
+        Topnav,
+    },
+};
 </script>
 <style lang="scss" scoped>
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions {
+        padding: 8px 0;
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
+    }
+}
 </style>
