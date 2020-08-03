c96816e12ed402989fdc413c0d9de3d0e86d1697
liuzhe <liuzhexx@foxmail.com>
2020-08-03 23:07:45 +0800

    switch的button样式美化

--- src/libs/Switch.vue
+++ src/libs/Switch.vue
@@ -1,51 +1,63 @@
 <template>
-  <button :class="{checked: value}" @click="toggleChecked"><span></span></button>
-  {{value}}
+    <button :class="{ checked: value }" @click="toggleChecked">
+        <span></span>
+    </button>
+    {{ value }}
 </template>
 <script lang="ts">
-import {ref} from 'vue'
+import { ref } from "vue";
 export default {
-  props: {
-    value: {
-      type: Boolean
-    }
-  },
-  setup(props, context) {
-    const toggleChecked = () => {
-      context.emit('update:value', !props.value)
-    }
-    return {toggleChecked}
-  }
-}
+    props: {
+        value: {
+            type: Boolean,
+        },
+    },
+    setup(props, context) {
+        const toggleChecked = () => {
+            context.emit("update:value", !props.value);
+        };
+        return { toggleChecked };
+    },
+};
 </script>
 
-
 <style lang="scss" scoped>
-  button {
+button {
     width: 40px;
     height: 20px;
-    background: gray;
+    background: #bfbfbf;
     border-radius: 20px;
     position: relative;
-    transition: background 250ms;
+    transition: all 250ms;
     span {
-      position: absolute;
-      left: 2px;
-      top: 2px;
-      width: 14px;
-      height: 14px;
-      border-radius: 50%;
-      background: #fff;
-      transition: left 250ms;
+        position: absolute;
+        left: 2px;
+        top: 2px;
+        width: 14px;
+        height: 14px;
+        border-radius: 50%;
+        background: #fff;
+        transition: all 250ms;
     }
     &.checked {
-      background: blue;
-      > span {
-        left: calc(100% - #{20px} + 2px)
-      }
+        background: #1890ff;
+        > span {
+            left: calc(100% - #{20px} + 2px);
+        }
     }
-  }
-  button:focus {
+}
+button:focus {
     outline: none;
-  }
+}
+button:active {
+    > span {
+        width: 14px + 4px;
+    }
+}
+button.checked:active {
+    > span {
+        width: 14px + 4px;
+        margin-left: -4px;
+    }
+}
 </style>
