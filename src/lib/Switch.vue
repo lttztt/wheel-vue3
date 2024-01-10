c0cd1c21921c095af96bf7911b840533d2fe6cfc
liuzhe <liuzhexx@foxmail.com>
2024-01-10 14:21:06 +0800

    update package

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,21 +1,21 @@
 <template>
-    <button :class="{ checked: value }" @click="toggleChecked">
-        <span></span>
-    </button>
-    {{ value }}
+  <button :class="{ checked: value }" @click="toggleChecked">
+    <span></span>
+  </button>
+  {{ value }}
 </template>
 <script lang="ts">
 import { ref } from "vue";
 export default {
-    props: {
-        value: Boolean,
-    },
-    setup(props, context) {
-        const toggleChecked = () => {
-            context.emit("update:value", !props.value);
-        };
-        return { toggleChecked };
-    },
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggleChecked = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggleChecked };
+  },
 };
 </script>
 
@@ -23,42 +23,42 @@ export default {
 $h: 22px;
 $h2: $h - 4px;
 button {
-    height: $h;
-    width: $h * 2;
-    border: none;
-    background: #bfbfbf;
-    border-radius: $h / 2;
-    position: relative;
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: calc($h / 2);
+  position: relative;
+  transition: all 250ms;
+  span {
+    position: absolute;
+    left: 2px;
+    top: 2px;
+    width: $h2;
+    height: $h2;
+    border-radius: calc($h / 2);
+    background: #fff;
     transition: all 250ms;
-    span {
-        position: absolute;
-        left: 2px;
-        top: 2px;
-        width: $h2;
-        height: $h2;
-        border-radius: $h2 / 2;
-        background: #fff;
-        transition: all 250ms;
+  }
+  &.checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
     }
-    &.checked {
-        background: #1890ff;
-        > span {
-            left: calc(100% - #{$h2} - 2px);
-        }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
     }
-    &:focus {
-        outline: none;
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
     }
-    &:active {
-        > span {
-            width: $h2 + 4px;
-        }
-    }
-    &.checked:active {
-        > span {
-            width: $h2 + 4px;
-            margin-left: -4px;
-        }
-    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,50 @@
 <template>
-    <div>
-        <Topnav />
-        <div class="banner" v-if="menuVisible">
-            <h1>禅UI</h1>
-            <h2>一个随性的UI框架</h2>
-            <p class="actions">
-                <a href="https://github.com" target="_bank">Github</a>
-                <router-link to="/doc">开始</router-link>
-            </p>
-        </div>
+  <div>
+    <Topnav />
+    <div class="banner" v-if="menuVisible">
+      <h1>禅UI</h1>
+      <h2>一个随性的UI框架</h2>
+      <p class="actions">
+        <a href="https://github.com" target="_bank">Github</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
     </div>
+  </div>
 </template>
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
-import {inject, Ref} from 'vue'
+import { inject, Ref } from "vue";
 export default {
-    name: 'Home',
-    components: {
-        Topnav,
-    },
-    setup() {
-      const menuVisible = inject<Ref<boolean>>('menuVisible')
-      return {menuVisible}
-    }
+  name: "Home",
+  components: {
+    Topnav,
+  },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    return { menuVisible };
+  },
 };
 </script>
 <style lang="scss" scoped>
 .banner {
-    padding: 100px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background: lightgreen;
-    > .actions {
-        padding: 8px 0;
-        a {
-            margin: 0 8px;
-            background: #fff;
-            display: inline-block;
-            $h: 28px;
-            height: $h;
-            line-height: $h;
-            border-radius: $h/2;
-            padding: 0 8px;
-        }
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: calc($h / 2);
+      padding: 0 8px;
     }
+  }
 }
 </style>
